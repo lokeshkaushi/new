f1c10f595209ae84a9fa065a6a039faab2300aff
LOKESH KAUSHIK <106736111+lokeshkaushi@users.noreply.github.com>
2022-12-16 15:38:28 +0530

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,149 @@
+from statistics import mode
+from django.db import models
+from email_login import settings
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now
+
+# Create your models here.
+
+
+
+
+class Blog(models.Model):
+    tag_name = models.CharField(max_length=100,default='')
+    blog_name = models.CharField(max_length=200,default='')
+    created_date = models.DateTimeField(default=now)
+    update_date =models.DateTimeField(auto_now_add=now)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    images= models.ImageField(upload_to='images/',null = True,blank =True)
+    is_approved = models.BooleanField(default=False)
+    def __str__(self):
+        return self.blog_name[0:20]+'....'+'by '+self.user.email
+
+
+
+class Post(models.Model):
+    post_name =models.CharField(max_length=100,default='')
+    tag_name = models.CharField(max_length=100,default='')
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    post_header= models.CharField(max_length=500,default='')
+    post_content= models.CharField(max_length=500,default='')
+    images= models.ImageField(upload_to='images/',blank =True)
+    document =models.FileField(upload_to='File/',blank =True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='likes',blank=True)
+    created_date = models.DateTimeField(default=now)
+    update_date = models.DateTimeField(auto_now_add=now)
+    is_active = models.BooleanField(default=True)
+    is_approved = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post_name+'...'+'by'+ self.user.email
+
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.images.url
+        except:
+            url = ''
+        return url
+
+    @property
+    def documentURL(self):
+        try:
+            url = self.document.url
+        except:
+            url = ''
+        return url
+
+class Profile_Pic(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='users')
+    background_image = models.ImageField(upload_to='images/',null = True,blank = True)
+    images= models.ImageField(upload_to='images/',null = True,blank =True)
+    Post =  models.ForeignKey(Post,on_delete=models.CASCADE,related_name='userprofile')
+    
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.images.url 
+        except:
+            url = ''
+        return url 
+    @property
+    def backgroundURL(self):
+        try:
+            url = self.backgound_image.url
+        except:
+            url =''
+        return url
+
+    @property
+    def __str__(self):
+        return self.user+'...'+'by'+ self.user
+
+    def __str__(self):
+        return self.user.email 
+
+class CustomUser(AbstractUser):
+    Profile_Pic = models.ForeignKey(Profile_Pic,on_delete=models.CASCADE,related_name='users')
+    email = models.EmailField(_('email address'), unique=True)
+    number =models.CharField(max_length=20,null=True,blank=True)
+    forget_password_token= models.CharField(max_length=100)
+   
+
+class Comments(models.Model):
+    cid = models.AutoField(auto_created=True,primary_key=True)
+    text = models.CharField(max_length=500)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
+    Post =  models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comment')
+    
+    def __str__(self):
+        return str(self.text)
+    def __str__(self):
+        return self.text[0:10]+'...'+'by '+ self.user.email
+
+class Reply(models.Model):
+    rid = models.AutoField(auto_created=True,primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    Comments=  models.ForeignKey(Comments,on_delete=models.CASCADE,related_name='reply') 
+    content =  models.CharField(max_length=500)
+    datetime = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.content)
+
+
+
+class Social(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,primary_key=True)
+    linkedin = models.URLField(max_length=500,null=True,blank=True)
+    twitter= models.URLField(max_length=500,null=True,blank=True)
+    instagram = models.URLField(max_length=500,null=True,blank=True)
+    facebook = models.URLField(max_length=500,null=True,blank=True)
+      
+    def __str__(self):
+        return self.user.email
+
+class About(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,primary_key=True)
+    description = models.CharField(max_length=500,null=True,blank=True)
+    location = models.CharField(max_length=100,null=True,blank=True)
+    email= models.EmailField(max_length=100,null=True,blank=True)
+    workad_at = models.CharField(max_length=100,null=True,blank=True)
+    Studied_at = models.CharField(max_length=100,null=True,blank=True)
+
+    def __str__(self):
+        return self.user.email
+        
+class change_password(models.Model):
+    new_password = models.CharField(max_length=500)
+    confirm_password = models.CharField(max_length=500)
+
+
+class Video(models.Model):
+    video = models.video = models.FileField(upload_to='videos_uploaded',null=True)
+    
---
+++ accounts/serializers.py
@@ -0,0 +1,119 @@
+
+from rest_framework import serializers
+from .models import *
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+
+
+class customuser_serializer(serializers.ModelSerializer):
+    class Meta:
+        model= CustomUser
+        fields = ['email','number','forget_password_token']
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        max_length=128, min_length=6, write_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('email','username','password','first_name','last_name','number')
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+
+
+
+class LoginSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        max_length=128, min_length=6, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password')
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    default_error_message = {
+        'bad_token': ('Token is expired or invalid')
+    }
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+
+        try:
+            RefreshToken(self.token).blacklist()
+
+        except TokenError:
+            self.fail('bad_token')        
+
+class forget_password_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('email',) 
+
+class Reset_password_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = change_password
+        fields =('new_password','confirm_password')
+
+class Social_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Social
+        fields ='__all__'
+
+
+class About_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields ='__all__'
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['rid','user','Comments','content','datetime']
+    
+
+class CommentsSerializer(serializers.ModelSerializer):
+    reply = ReplySerializer(many=True,read_only=True)
+    
+    class Meta:
+        model = Comments
+        fields =['cid','text','user','datetime','Post','reply']
+
+
+class Profile_Pic_serializer(serializers.ModelSerializer):
+    users= RegisterSerializer(many=True,read_only =True)
+    class Meta:
+        model = Profile_Pic
+        fields =['id','user','users','background_image','images']
+
+class User_Post_serializer(serializers.ModelSerializer):
+    userprofile = Profile_Pic_serializer(many=True,read_only=True)
+    comment = CommentsSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Post
+        fields = ['user','post_name','tag_name','blog','post_header','post_content','images','document','likes','created_date','update_date','is_active','userprofile','comment']
+        
+ 
+class BlogSerializer(serializers.ModelSerializer):
+    group_set = Profile_Pic_serializer(many=True, read_only =True)
+    class Meta:
+        model = Blog
+        fields = ['id','tag_name','blog_name','created_date','update_date','user','images','group_set']
+        
+    def create(self, validated_data):
+        return Blog.objects.create(**validated_data)
+
+
+class Videoserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields ='__all__'
